56120f51125b0863913217189feccde736aac26d
MarkusIppy <Markus.Ippy1981@gmail.com>
2017-11-07 10:39:01 +0200

    Update buildQT5.9.sh

--- buildQT5.9.sh
+++ buildQT5.9.sh
@@ -18,47 +18,36 @@ tar xf qt-everywhere-opensource-src-5.9.2.tar.xz
 # Delete source tarball to save some space
 sudo rm -r qt-everywhere-opensource-src-5.9.2.tar.xz
 
+# Delete qmake.conf for Raspberry Pi2 
+cd /home/pi/qt-everywhere-opensource-src-5.9.2/qtbase/mkspecs/devices/linux-rasp-pi2-g++
+sudo rm qmake.conf
+
+# Download modified qmake.conf for Raspberry Pi2
+wget https://github.com/MarkusIppy/QT5.9.2-raspbian-stretch/qmake.conf
+
 # Create Shadow build directory 
+cd
 mkdir build
 cd build
 
 
-# ***************************************************************************
-# We unfortunatly have to fix some librarynames.
-#sudo cp /opt/vc/lib/libGLESv2.so /opt/vc/lib/libGLESv2.so.bak
-#sudo cp /opt/vc/lib/libEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.bak
-
-#sudo mv /opt/vc/lib/libGLESv2.so /opt/vc/lib/libGLESv2.so.2
-#sudo mv /opt/vc/lib/libEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1
-
-# Fix libraries (not sure if this is needed) 
-# sudo ln -fs /opt/vc/lib/libGLESv2.so /opt/vc/lib/libGLESv2.so.2
-# sudo ln -fs /opt/vc/lib/libEGL.so /opt/vc/lib/libEGL.so.1
-# sudo ldconfig
-# ***************************************************************************
-
-
-
-# Uncomment the configuration for your raspberry PI version (Default is PI2)
-
-# Raspberry PI Configuration ARMv6
-#PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig PKG_CONFIG_SYSROOT_DIR=/ \../qt-everywhere-opensource-src-5.9.2/configure -v -opengl es2 -eglfs -no-gtk -device linux-rasp-pi-g++ \-device-option CROSS_COMPILE=/usr/bin/ -opensource -confirm-license -reduce-exports \-force-pkg-config -nomake examples -no-compile-examples -skip qtwayland -skip qtwebengine -release \-qt-pcre -ssl -evdev -system-freetype -fontconfig -glib -prefix /opt/Qt5.9
-
-# Raspberry PI2 Configuration ARMv7 (default)
+# Configure QT for Raspberry PI2 ARMv7
 PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig PKG_CONFIG_SYSROOT_DIR=/ \../qt-everywhere-opensource-src-5.9.2/configure -v -opengl es2 -eglfs -no-gtk -device linux-rasp-pi2-g++ \-device-option CROSS_COMPILE=/usr/bin/ -opensource -confirm-license -reduce-exports \-force-pkg-config -nomake examples -no-compile-examples -skip qtwayland -skip qtwebengine -release \-qt-pcre -ssl -evdev -system-freetype -fontconfig -glib -prefix /opt/Qt5.9
 
-# Raspberry PI3 Configuration ARMv8
-#PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig PKG_CONFIG_SYSROOT_DIR=/ \../qt-everywhere-opensource-src-5.9.2/configure -v -opengl es2 -eglfs -no-gtk -device linux-rasp-pi3-g++ \-device-option CROSS_COMPILE=/usr/bin/ -opensource -confirm-license -reduce-exports \-force-pkg-config -nomake examples -no-compile-examples -skip qtwayland -skip qtwebengine -release \-qt-pcre -ssl -evdev -system-freetype -fontconfig -glib -prefix /opt/Qt5.9
 
-
-# Compile QT with 4 cores ( remove -j4 if building on Pi)
+# Compile QT with 4 cores 
 make -j4
 
 
 # Install QT on the system 
 make install
 
-#Delete shadow build directory (optional) 
-#cd
-#sudo rm -r build
+# Delete shadow build directory
+cd
+sudo rm -r build
+
+# Delete source code folder
+sudo rm -r qt-everywhere-opensource-src-5.9.2
+
+
 

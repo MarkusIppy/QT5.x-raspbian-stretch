baaee29c07efe330804be66e846858e7da8dcd78
MarkusIppy <Markus.Ippy1981@gmail.com>
2017-11-09 09:36:20 +0200

    A quick script to recompile and reinstall all qt modules seperately

---
+++ fixqtinstall.sh
@@ -0,0 +1,128 @@
+cd /home/pi/build/qtbase
+make -j4
+sudo make install
+
+cd /home/pi/build/qt3d
+make -j4
+sudo make install
+
+cd /home/pi/build/qtactiveqt
+make -j4
+sudo make install
+
+cd /home/pi/build/qtcanvas3d
+make -j4
+sudo make install
+
+cd /home/pi/build/qtdeclarative
+make -j4
+sudo make install
+
+cd /home/pi/build/qtdoc
+make -j4
+sudo make install
+
+cd /home/pi/build/qtgamepad
+make -j4
+sudo make install
+
+cd /home/pi/build/qtgraphicaleffects
+make -j4
+sudo make install
+
+cd /home/pi/build/qtimageformats
+make -j4
+sudo make install
+
+cd /home/pi/build/qtlocation
+make -j4
+sudo make install
+
+cd /home/pi/build/qtmultimedia
+make -j4
+sudo make install
+
+cd /home/pi/build/qtnetworkauth
+make -j4
+sudo make install
+
+cd /home/pi/build/qtscript
+make -j4
+sudo make install
+
+cd /home/pi/build/qtscxml
+make -j4
+sudo make install
+
+cd /home/pi/build/qtpurchasing
+make -j4
+sudo make install
+
+cd /home/pi/build/qtquickcontrols
+make -j4
+sudo make install
+
+cd /home/pi/build/qtquickcontrols2
+make -j4
+sudo make install
+
+cd /home/pi/build/qtsensors
+make -j4
+sudo make install
+
+cd /home/pi/build/qtserialbus
+make -j4
+sudo make install
+
+cd /home/pi/build/qtserialport
+make -j4
+sudo make install
+
+cd /home/pi/build/qtspeech
+make -j4
+sudo make install
+
+cd /home/pi/build/qtsvg
+make -j4
+sudo make install
+
+cd /home/pi/build/qttools
+make -j4
+sudo make install
+
+cd /home/pi/build/qttranslations
+make -j4
+sudo make install
+
+cd /home/pi/build/qtvirtualkeyboard
+make -j4
+sudo make install
+
+cd /home/pi/build/qtwebchannel
+make -j4
+sudo make install
+
+cd /home/pi/build/qtwebsockets
+make -j4
+sudo make install
+
+cd /home/pi/build/qtwebengine
+make -j4
+sudo make install
+
+cd /home/pi/build/qtwebview
+make -j4
+sudo make install
+
+cd /home/pi/build/qtcharts
+make -j4
+sudo make install
+
+cd /home/pi/build/qtconnectivity
+make -j4
+sudo make install
+
+cd /home/pi/build/qtdatavis3d
+make -j4
+sudo make install
+
